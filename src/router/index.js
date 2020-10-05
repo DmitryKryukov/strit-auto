64a79429afa7c532d6ac56272592d16f0a848fb5
DmitryKryukov <demobbtw@gmail.com>
2020-10-06 00:08:19 +0400

    =Model page component draft

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home";
 import Cars from "@/components/Cars";
+import Model from "@/components/Model";
 Vue.use(Router);
 
 export default new Router({
@@ -16,5 +17,10 @@ export default new Router({
       name: "Cars",
       component: Cars
     },
+    {
+      path: "/cars/model",
+      name: "Model",
+      component: Model
+    }
   ]
 });
